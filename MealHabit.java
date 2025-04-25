91cc30070d47a4782f6fc3b1e2a727dbf39d5064
amberrama <amberrama06@gmail.com>
2025-04-25 01:37:03 -0700

    Update MealHabit.java

--- MealHabit.java
+++ MealHabit.java
@@ -1,15 +1,18 @@
-public class FitnessHabit extends Habit {
-    private String type;
-    private int duration; // in minutes
+public class MealHabit extends Habit {
+    private String mealType; // e.g. Breakfast, Lunch, Dinner
+    private int calories;
 
-    public FitnessHabit(String name, String date, String type, int duration) {
+    public MealHabit(String name, String date, String mealType, int calories) {
         super(name, date);
-        this.type = type;
-        this.duration = duration;
+        this.mealType = mealType;
+        this.calories = calories;
     }
 
     @Override
     public void markComplete() {
         this.isComplete = true;
     }
+
+    public String getMealType() { return mealType; }
+    public int getCalories() { return calories; }
 }
