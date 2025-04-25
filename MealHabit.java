c94469c1ce8b34cd9fa9365651888206d26497c1
amberrama <amberrama06@gmail.com>
2025-04-25 01:13:05 -0700

    Create MealHabit.java

---
+++ MealHabit.java
@@ -0,0 +1,15 @@
+public class FitnessHabit extends Habit {
+    private String type;
+    private int duration; // in minutes
+
+    public FitnessHabit(String name, String date, String type, int duration) {
+        super(name, date);
+        this.type = type;
+        this.duration = duration;
+    }
+
+    @Override
+    public void markComplete() {
+        this.isComplete = true;
+    }
+}
