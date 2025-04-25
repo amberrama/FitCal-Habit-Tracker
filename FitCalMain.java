be1a4f997e9cdd39dd5a01e35f6f03c2550a321a
amberrama <amberrama06@gmail.com>
2025-04-25 01:14:10 -0700

    Create FitCalMain.java

---
+++ FitCalMain.java
@@ -0,0 +1,6 @@
+public class FitCalMain {
+    public static void main(String[] args) {
+        // Placeholder for launching GUI (to be added)
+        System.out.println("Welcome to FitCal! (GUI coming soon)");
+    }
+}
