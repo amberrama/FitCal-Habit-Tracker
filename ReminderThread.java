e2216f72e9bbcb24a41d5b4ad46d46ebad7501d2
amberrama <amberrama06@gmail.com>
2025-04-25 01:41:15 -0700

    Create ReminderThread.java

---
+++ ReminderThread.java
@@ -0,0 +1,20 @@
+public class ReminderThread extends Thread {
+    private boolean running = true;
+
+    public void run() {
+        while (running) {
+            try {
+                // Placeholder: Print reminder every 10 seconds
+                System.out.println("Don't forget to log your habits!");
+                Thread.sleep(10000); // 10 seconds for testing
+            } catch (InterruptedException e) {
+                System.out.println("Reminder thread interrupted.");
+                running = false;
+            }
+        }
+    }
+
+    public void stopReminders() {
+        running = false;
+    }
+}
