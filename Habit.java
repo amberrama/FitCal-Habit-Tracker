06601ae89069ffd702343e5958cb6ddf3a0481e1
amberrama <amberrama06@gmail.com>
2025-04-25 01:11:32 -0700

    Create Habit.java

---
+++ Habit.java
@@ -0,0 +1,17 @@
+public abstract class Habit {
+    protected String name;
+    protected String date;
+    protected boolean isComplete;
+
+    public Habit(String name, String date) {
+        this.name = name;
+        this.date = date;
+        this.isComplete = false;
+    }
+
+    public abstract void markComplete();
+
+    public String getName() { return name; }
+    public String getDate() { return date; }
+    public boolean isComplete() { return isComplete; }
+}
