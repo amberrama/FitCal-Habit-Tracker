7aee46f7992c9d56130aae53f7fbaaf928b7919a
amberrama <amberrama06@gmail.com>
2025-04-25 01:41:35 -0700

    Update FitCalMain.java

--- FitCalMain.java
+++ FitCalMain.java
@@ -1,6 +1,22 @@
+import javax.swing.*;
+
 public class FitCalMain {
     public static void main(String[] args) {
-        // Placeholder for launching GUI (to be added)
-        System.out.println("Welcome to FitCal! (GUI coming soon)");
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JFrame frame = new JFrame("FitCal - Habit Tracker");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setSize(600, 400);
+
+                CalendarView calendarPanel = new CalendarView();
+                frame.add(calendarPanel);
+
+                frame.setVisible(true);
+            }
+        });
+
+        // Start reminder thread (can close with app exit)
+        ReminderThread reminder = new ReminderThread();
+        reminder.start();
     }
 }
